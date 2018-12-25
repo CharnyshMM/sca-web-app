489ddbb2a162251858d5e0a206aae557734372f4
Mikita Charnysh <mr.nick.cher@gmail.com>
2018-12-25 18:28:57 +0300

    better result output in CustomQuery

--- sca_web_front/src/CustomQuery/CustomQuery.jsx
+++ sca_web_front/src/CustomQuery/CustomQuery.jsx
@@ -32,12 +32,12 @@ class CustomQuery extends Component {
   render() {
 
     const parseClauses = () => {
-      const queryText = this.state.clauses.map(clause => `${{
-        match: 'MATCH',
-        return: 'RETURN',
-        where: 'WHERE',
-        limit: 'LIMIT',
-        orderby: 'ORDER BY',
+      const queryText = this.state.clauses.map(clause => ` ${{
+        match: ' MATCH ',
+        return: ' RETURN ',
+        where: ' WHERE ',
+        limit: ' LIMIT ',
+        orderby: ' ORDER BY ',
       }[clause.type]} ${clause.value}`).join('\n');
       this.setState(prev => ({ queryText }));
       this.queryEditor.setValue(queryText);
@@ -49,70 +49,89 @@ class CustomQuery extends Component {
     }
 
     const removeClause = index => () => {
-      this.setStatePromise(prev => ({ clauses: [
-        ...prev.clauses.slice(0, index),
-        ...prev.clauses.slice(index + 1),
-      ] }))
+      this.setStatePromise(prev => ({
+        clauses: [
+          ...prev.clauses.slice(0, index),
+          ...prev.clauses.slice(index + 1),
+        ]
+      }))
         .then(parseClauses);
     }
 
     const changeClauseType = index => e => {
       const type = e.target.value;
-      this.setStatePromise(prev => ({ clauses: [
-        ...prev.clauses.slice(0, index),
-        { ...prev.clauses[index], type },
-        ...prev.clauses.slice(index + 1),
-      ] }))
+      this.setStatePromise(prev => ({
+        clauses: [
+          ...prev.clauses.slice(0, index),
+          { ...prev.clauses[index], type },
+          ...prev.clauses.slice(index + 1),
+        ]
+      }))
         .then(parseClauses);
     }
 
     const changeClauseValue = index => e => {
       const value = e.target.value;
-      this.setStatePromise(prev => ({ clauses: [
-        ...prev.clauses.slice(0, index),
-        { ...prev.clauses[index], value },
-        ...prev.clauses.slice(index + 1),
-      ] }))
+      this.setStatePromise(prev => ({
+        clauses: [
+          ...prev.clauses.slice(0, index),
+          { ...prev.clauses[index], value },
+          ...prev.clauses.slice(index + 1),
+        ]
+      }))
         .then(parseClauses);
     }
-    
+
     const changeQueryText = event => {
-        this.setState({ queryText: event.target.value });
+      this.setState({ queryText: event.target.value });
     };
 
     const handleSubmit = e => {
-        e.preventDefault();
-        let status = 0;
-        const token = window.sessionStorage.getItem("token");
-        this.setState({ error: undefined, result: undefined });
-        runQueryOnPythonBackend(this.queryEditor.getValue(), token)
-          .then(result => {
-                status = result.status;
-                return result.response.json();
-              },
-              error => {
-                status = error.status;
-                return error.response.json();
-              }) 
-          .then(result => {
-                console.log('responsed_Custom_query:', result, status);
-                  if (status == 200) {
-                      this.setState({ result: result });
-                  } else {
-                      console.log("I throwed an error");
-                      throw Error(result.error);
-                  }
-              },
-          )
-          .catch(e => {
-            this.setState({ error: e});
-            console.log("ERROR:", e);
-          });
+      e.preventDefault();
+      let status = 0;
+      const token = window.sessionStorage.getItem("token");
+      this.setState({ error: undefined, result: undefined });
+      runQueryOnPythonBackend(this.queryEditor.getValue(), token)
+        .then(result => {
+          status = result.status;
+          return result.response.json();
+        },
+          error => {
+            status = error.status;
+            return error.response.json();
+          })
+        .then(result => {
+          console.log('responsed_Custom_query:', result, status);
+          if (status == 200) {
+            this.setState({ result: result });
+          } else {
+            console.log("I throwed an error");
+            throw Error(result.error);
+          }
+        },
+      )
+        .catch(e => {
+          this.setState({ error: e });
+          console.log("ERROR:", e);
+        });
     }
-    
+
     const toggleConstructor = e => {
-        this.setState({ useConstructor: e.target.checked });
-        this.queryEditor.setOption('readOnly', e.target.checked);
+      this.setState({ useConstructor: e.target.checked });
+      this.queryEditor.setOption('readOnly', e.target.checked);
+    }
+
+    const parseResultItem = (item) => {
+      let viewContent = "";
+
+      viewContent = (<ul style={{ maxWidth: '70%' }}>
+        {Object.keys(item).map((key, i) =>
+          <li>
+            <b>{key}:</b> {item[key]}
+          </li>
+        )}
+      </ul>);
+      return viewContent;
     }
 
     return (
@@ -120,10 +139,10 @@ class CustomQuery extends Component {
         <h1>Query builder</h1>
         <form className="form" onSubmit={handleSubmit}>
           <div className={this.state.useConstructor ? 'form-group disabled' : 'form-group'}>
-            <textarea className="form-control" value={this.state.queryText} onChange={changeQueryText}/>
+            <textarea className="form-control" value={this.state.queryText} onChange={changeQueryText} />
           </div>
           <div className="form-check">
-            <input type="checkbox" checked={this.state.useConstructor} onChange={toggleConstructor} className="form-check-input" id="use-constructor"/>
+            <input type="checkbox" checked={this.state.useConstructor} onChange={toggleConstructor} className="form-check-input" id="use-constructor" />
             <label htmlFor="use-constructor" className="form-check-label">Use Constructor</label>
           </div>
           {this.state.useConstructor && (
@@ -144,7 +163,7 @@ class CustomQuery extends Component {
                           </select>
                         </div>
                         <div className="input-group">
-                          <input type="text" value={clause.value} onChange={changeClauseValue(i)} className="form-control"/>
+                          <input type="text" value={clause.value} onChange={changeClauseValue(i)} className="form-control" />
                         </div>
                       </div>
                       <button type="button" className="close" onClick={removeClause(i)}>
@@ -172,7 +191,7 @@ class CustomQuery extends Component {
                 <table className="table table-sm">
                   <tbody>
                     {Object.keys(record).map((key, j) => (
-                      <tr key={j}><th scope="row" style={{ width: '50%' }}>{key}</th><td>{JSON.stringify(record[key])}</td></tr>
+                      <tr key={j}><th scope="row">{key}</th><td style={{ maxWidth: '70%' }}>{parseResultItem(record[key])}</td></tr>
                     ))}
                   </tbody>
                 </table>
@@ -187,6 +206,6 @@ class CustomQuery extends Component {
 
 export default props => (
   <NeoContext.Consumer>
-    {({ connection, is_admin }) => <CustomQuery {...props} connection={connection} is_admin={is_admin}/>}
+    {({ connection, is_admin }) => <CustomQuery {...props} connection={connection} is_admin={is_admin} />}
   </NeoContext.Consumer>
 );

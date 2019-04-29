877b6c7d6f9d88bcfdcb817cd8ab591d46e0369c
Mikita Charnysh <mr.nick.cher@gmail.com>
2019-04-29 13:14:02 +0300

    replace state with useState hook

--- sca_web_front/src/Home/Home.jsx
+++ sca_web_front/src/Home/Home.jsx
@@ -1,77 +1,72 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 import Feature from './Feature';
-import {createSearchLink} from '../utilities/links_creators';
+import { createSearchLink } from '../utilities/links_creators';
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      search_input: "",
-    };
-  }
+const Home = (props) => {
+  const [ searchInput, onSearchInputChange ] = useState("");
 
-  render() {
-    const isAdmin = window.sessionStorage.getItem("isAdmin") == "true";
+  const onSearchClick = e => {
+    e.preventDefault();
+    props.history.push(createSearchLink(searchInput, "all"));
+  };
 
-    const onSearchClick = e => {
-      e.preventDefault();
-      this.props.history.push(createSearchLink(this.state.search_input, "all"));
-    };
+  const isAdmin = window.sessionStorage.getItem("isAdmin") == "true";
 
-    const onSearchInputChange = (e) => {
-      this.setState({search_input: e.target.value});
-    }
+  return (
+    <div className='container'>
+      <h1>Dashboard</h1>
 
-    return (
-      <div className='container'>
-        <h1>Dashboard</h1>
+      <form onSubmit={onSearchClick}>
+        <div className="search_form">
+          <input
+            className="search_form__input"
+            value={searchInput}
+            type="text"
+            onChange={e => onSearchInputChange(e.target.value)}
+            placeholder="Search for knowledge..."
+          />
+          <button className="search_form__button" type="submit">Go!</button>
+        </div>
+      </form>
 
-        <form method="GET" onSubmit={onSearchClick}>
-          <div className="search_form">
-            <input className="search_form__input" value={this.state.search_input} type="text" onChange={onSearchInputChange} placeholder="Search for knowledge..." />
-            <button className="search_form__button btn btn-primary" type="submit">Go!</button>
-          </div>
-        </form>
-
-        <section className="features">
-          <Feature 
-            name="FIND EXPERTS"
-            iconClassName="oi oi-person"
-            href="/authorities-query"
-            >
-            in particular domains by selecting authors
-              with most cited publications
+      <section className="features">
+        <Feature
+          name="FIND EXPERTS"
+          iconClassName="oi oi-person"
+          href="/authorities-query"
+        >
+          in particular domains by selecting authors
+            with most cited publications
           </Feature>
-          <Feature 
-            name="EXPLORE DOMAINS"
-            iconClassName="oi oi-bar-chart"
-            href="/domains-query"
-            >
-            their popularity and publicing dynamics through the time
+        <Feature
+          name="EXPLORE DOMAINS"
+          iconClassName="oi oi-bar-chart"
+          href="/domains-query"
+        >
+          their popularity and publicing dynamics through the time
           </Feature>
-          {isAdmin && 
-          <Feature 
+        {isAdmin &&
+          <Feature
             name="MAKE QUERIES"
             iconClassName="oi oi-terminal"
             href="/custom-query"
-            >
+          >
             to the Graph of knowledge in Neo4j&trade; database by writing your own queries in CYPHER
           </Feature>
-          }
-          {isAdmin == false && 
-          <Feature 
-          name="ABOUT CAS"
-          iconClassName="oi oi-flag"
-          href="/custom-query"
+        }
+        {isAdmin == false &&
+          <Feature
+            name="ABOUT CAS"
+            iconClassName="oi oi-flag"
+            href="/custom-query"
           >
-          to the Graph of knowledge in Neo4j&trade; database by writing your own queries in CYPHER
+            to the Graph of knowledge in Neo4j&trade; database by writing your own queries in CYPHER
         </Feature>
-          }
-        </section>
-      </div>
-    );
-  }
+        }
+      </section>
+    </div>
+  );
 }
 
 export default Home;

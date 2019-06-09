084b0e02974586e5a69ecb7dc9d5418ecee2c2b9
Mikita Charnysh <mr.nick.cher@gmail.com>
2019-06-09 13:43:50 +0300

    add memoization & start hints

--- sca_web_front/src/ReusableComponents/BeautifulChart/BeautifulChart.jsx
+++ sca_web_front/src/ReusableComponents/BeautifulChart/BeautifulChart.jsx
@@ -5,20 +5,73 @@ import {
     YAxis,
     VerticalGridLines,
     HorizontalGridLines,
-    VerticalBarSeries
+    VerticalBarSeries,
+    Hint
 } from 'react-vis';
 
 import "./BeautifulChart.css";
 
+function getChartDataBuilder() {
+    console.log("memoized builder created")
+    let _valuesPerGroup = 1;
+    let _valueGroups = [];
+    let _data = undefined
+    return (valuesPerGroup, data) => {
+        if (_valuesPerGroup == valuesPerGroup && _data == data) {
+            return _valueGroups
+        }
+        _valuesPerGroup = valuesPerGroup;
+        _data = data;
+        const currentColumnsCount = Math.floor(data.length / valuesPerGroup);
+        console.log("valuesPerGroup",valuesPerGroup);
+        console.log("currentColumnsCount",currentColumnsCount);
+
+        const valuesLeft = (data.length % valuesPerGroup);
+        console.log("valuesLeft",valuesLeft);
+        _valueGroups = [];
+        
+        for(let i = 0; i < data.length - valuesLeft;) {
+            let {x, y} = data[i];
+            let j = 0;
+            for (;j < valuesPerGroup; j++) {
+               y = y + data[i+j].y; 
+            }
+            if (x != data[i+j-1].x) {
+                x = `${x}-${data[i+j-1].x}`;
+            }
+            _valueGroups.push({x:x, y:y});
+            i = i + valuesPerGroup;
+        }
+
+        if (valuesLeft > 0) {
+            const startX = data[data.length - valuesLeft].x;
+            const endX = data[data.length-1].x;
+            const lastColumn = {
+                x: startX == endX ? startX : `${startX}-${endX}`,
+                y: 0
+            };
+            for (let i = data.length - valuesLeft; i < data.length; i++) {
+                lastColumn.y += data[i].y;
+            }
+            _valueGroups.push(lastColumn);
+        }
+        return _valueGroups;
+    };
+}
+
 const SINGLE_COLUMN_WIDTH = 50;
 const MAX_VALUES_PER_COLUMN = 10;
+
 
 class BeautifulChart extends Component {
     state = {
         valuesPerGroup: 1,
         plusEnabled: false,
-        minusEnabled: true
-    };
+        minusEnabled: true,
+        hintValue: undefined
+    }
+    
+    getChartData = getChartDataBuilder()
 
     onMinusClick = () => {
         const {valuesPerGroup} = this.state;
@@ -54,46 +107,22 @@ class BeautifulChart extends Component {
         }
     }
 
+    
+
+    onValueMouseOver = (datapoint, event) => {
+        // does something on click
+        // you can access the value of the event
+    }
+
     render() {
         const { data } = this.props;
-        const { valuesPerGroup, plusEnabled, minusEnabled } = this.state;
-        const currentColumnsCount = Math.floor(data.length / valuesPerGroup);
-        console.log("valuesPerGroup",valuesPerGroup);
-        console.log("currentColumnsCount",currentColumnsCount);
-
-        const valuesLeft = (data.length % valuesPerGroup);
-        console.log("valuesLeft",valuesLeft);
-        const valueGroups = [];
+        const { valuesPerGroup, plusEnabled, minusEnabled, hintValue } = this.state;
         
-        for(let i = 0; i < data.length - valuesLeft;) {
-            let {x, y} = data[i];
-            let j = 0;
-            for (;j < valuesPerGroup; j++) {
-               y = y + data[i+j].y; 
-            }
-            if (x != data[i+j-1].x) {
-                x = `${x}-${data[i+j-1].x}`;
-            }
-            valueGroups.push({x:x, y:y});
-            i = i + valuesPerGroup;
-        }
+        const chartData = this.getChartData(valuesPerGroup, data);
 
-        if (valuesLeft > 0) {
-            const startX = data[data.length - valuesLeft].x;
-            const endX = data[data.length-1].x;
-            const lastColumn = {
-                x: startX == endX ? startX : `${startX}-${endX}`,
-                y: 0
-            };
-            for (let i = data.length - valuesLeft; i < data.length; i++) {
-                lastColumn.y += data[i].y;
-            }
-            valueGroups.push(lastColumn);
-        }
-
-        const chartWidth = SINGLE_COLUMN_WIDTH*valueGroups.length;
+        const chartWidth = SINGLE_COLUMN_WIDTH*chartData.length;
         return (
-            <section className="beautiful_chart">
+            <section >
                 {/* <p>valuesPerGroup = {valuesPerGroup}, data.length= {data.length} valuesLeft= {valuesLeft}, length = {valueGroups.length}</p> */}
                 <button disabled={!plusEnabled} onClick={this.onPlusClick}>
                     <span className="oi oi-zoom-in"> </span>
@@ -101,21 +130,28 @@ class BeautifulChart extends Component {
                 <button disabled={!minusEnabled} onClick={this.onMinusClick}>
                     <span className="oi oi-zoom-out"> </span>
                 </button>
-                <XYPlot 
-                    margin={{ bottom: 70 }}
-                     xType="ordinal" 
-                     width={chartWidth} 
-                     height={200} 
-                     >
-                    <VerticalGridLines />
-                    <HorizontalGridLines />
-                    <YAxis />
-                    <XAxis tickLabelAngle={-45}/>
-                    <VerticalBarSeries
-                        barWidth={0.4}
-                        data={valueGroups}
-                    />
-                </XYPlot>
+                <div className="beautiful_chart">
+                    <XYPlot 
+                        margin={{ bottom: 70 }}
+                        xType="ordinal" 
+                        width={chartWidth} 
+                        height={200}
+                        
+                        onMouseLeave={() => this.setState({value: undefined})} 
+                        >
+                        <VerticalGridLines />
+                        <HorizontalGridLines />
+                        <YAxis />
+                        <XAxis tickLabelAngle={-45}/>
+                        <VerticalBarSeries
+                            barWidth={0.4}
+                            data={chartData}
+                            onValueMouseOver={datapoint => console.log(datapoint)}
+                            onNearestXY={hintValue => this.setState({hintValue})}
+                        />
+                        {hintValue && <Hint value={hintValue} />}
+                    </XYPlot>
+                </div>
             </section>
         )
     }
